7a972d2019f28c50f53484f86b58e1269f79d833
francescoP02 <file.partipilo@gmail.com>
2022-05-10 15:58:42 +0200

    Milestone1

--- js/data.js
+++ js/data.js
@@ -1,3 +1,8 @@
+// Oggi iniziamo a realizzare Boolzapp - a (not very) innovative messaging:
+// Il compito è di implementare la MILESTONE 1:
+// --> Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// -->  Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
+
 var app = new Vue(
     {
         el: `#root`,
