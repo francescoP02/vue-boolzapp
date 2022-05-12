5412276e37c34661db92f13513de039db0c40a73
francescoP02 <file.partipilo@gmail.com>
2022-05-12 12:28:11 +0200

    Added dropdown menu with delete function

--- js/data.js
+++ js/data.js
@@ -27,17 +27,20 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         }
                     ],
                 },
@@ -49,17 +52,20 @@ var app = new Vue(
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         }
                     ],
                 },
@@ -71,17 +77,20 @@ var app = new Vue(
                         {
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         }
                     ],
                 },
@@ -93,12 +102,14 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         }
                     ],
                 },
@@ -110,12 +121,14 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         }
                     ],
                 },
@@ -127,17 +140,20 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         }
                     ],
                 },
@@ -149,12 +165,14 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         }
                     ],
                 },
@@ -166,17 +184,20 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
+                            status: 'sent',
+                            dropdownmenu: `false`,
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
-                            status: 'received'
+                            status: 'received',
+                            dropdownmenu: `false`,
                         }
                     ],
                 }
@@ -191,7 +212,7 @@ var app = new Vue(
 
             addMessage : function(contacts ,currentContact) {
                 if (this.newMessageInput !== "") {
-                    this.contacts[currentContact].messages.push({message: this.newMessageInput, status: `sent`, date: this.getCurrentTime()});
+                    this.contacts[currentContact].messages.push({message: this.newMessageInput, status: `sent`, date: this.getCurrentTime(), dropdownmenu: `false`,});
                     this.newMessageInput = ``;
                     this.generateMsg(contacts, currentContact);
                 }
@@ -199,7 +220,7 @@ var app = new Vue(
 
             generateMsg(contacts, currentContact) {
                 this.autoMessage = setTimeout( () => {
-                    this.contacts[currentContact].messages.push({message: "Ok", status: `received`, date: this.getCurrentTime()});
+                    this.contacts[currentContact].messages.push({message: "Ok", status: `received`, date: this.getCurrentTime(), dropdownmenu: `false`,});
                 }, 1000);
             },
 
@@ -229,6 +250,17 @@ var app = new Vue(
             getTime(date) {
                 const dayjsDate = dayjs(date, 'DD-MM-YYYY HH:mm:ss');
                 return dayjsDate.format('HH:mm:ss');
+            },
+
+            dropdownMenu(item) {
+                if (item.dropdownmenu == `false`) {
+                    item.dropdownmenu = `true`;
+                } else item.dropdownmenu = `false`;
+                console.log(item.dropdownmenu);
+            },
+
+            deleteMessage(index) {
+                this.contacts[this.currentContact].messages.splice(index,1);
             },
 
         },
